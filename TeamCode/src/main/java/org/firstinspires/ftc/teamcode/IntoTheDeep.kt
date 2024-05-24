0ebc8806280fad0557500bf33f966711e6477a05
Hivemind Members <hivemindmembers@itzpolar.me>
2024-05-24 17:34:28 -0500

    swarm works on workstation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep.kt
@@ -1,7 +1,15 @@
 package org.firstinspires.ftc.teamcode
 
+import net.hivemindrobotics.swarm.hardware.motor.factory.MotorFactory
 import net.hivemindrobotics.swarm.math.Pose
 
 class IntoTheDeep(pose: Pose) {
-    // TODO
+    private val frontLeftMotor = MotorFactory("frontLeftMotor")
+            .reverse
+            .brake
+            .build()
+
+    init {
+        frontLeftMotor.power = 1.0
+    }
 }
